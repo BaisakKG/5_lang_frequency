a04a28bd8882cec76f93d3d4cf739d77e0838267
BaisakKG <baisak40@gmail.com>
2017-12-08 11:40:27 +0600

    Use module collections.counter

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,4 +1,4 @@
-import operator
+from collections import Counter
 
 def load_data(filepath):
     with open(filepath, 'r') as file_handler:
@@ -6,17 +6,11 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(file_text):
-    words = dict()
+    cleaned_list = []
     for word in file_text.split():
-        cleaned_word = word.strip('.,!-:"\()\'').lower()
-        if cleaned_word not in words:
-            words[cleaned_word] = 0
-        words[cleaned_word] += 1
-    
-    word_items = words.items()
-    word_count_items = sorted(word_items, key=operator.itemgetter(1), reverse=True)
-    print(word_count_items[:10])
-
+        cleaned_list.append(word.strip('.,!-:"\()\'').lower())
+        
+    print(Counter(cleaned_list).most_common(10))
 
 if __name__ == '__main__':
     file_text = load_data(r'd:\PythonScript\Devman\RomeoAndJuliet.txt')
