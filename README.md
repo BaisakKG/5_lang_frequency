3af79c519a233f2ff92e3b150e37571aad5ca87f
BaisakKG <baisak40@gmail.com>
2017-12-08 10:59:20 +0600

    Write readme and code end

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script accepts a way to the text file on an input and to print in the console ten most popular words in this file in decreasing order of frequency.
+
+# Quickstart
+
+Example use as module:
+
+```python
+import lang_frequency
+
+file = load_data(r'd:\PythonScript\Devman\RomeoAndJuliet.txt')
+get_most_frequent_words(file)
+
+# Output example
+[('the', 865), ('and', 787), ('to', 622), ('i', 582), ('of', 533), ('a', 524), ('in', 375), ('is', 372), ('my', 360), ('that', 358)]
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,24 @@
-
+import operator
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_handler:
+        return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = dict()
+    for word in text.split():
+        cleaned_word = word.strip('.,!-:"\()\'').lower()
+        if cleaned_word not in words:
+            words[cleaned_word] = 0
+        words[cleaned_word] += 1
+    
+    word_items = words.items()
+    word_count_items = sorted(word_items, key=operator.itemgetter(1), reverse=True)
+    print(word_count_items[:10])
 
 
 if __name__ == '__main__':
-    pass
+    file = load_data(r'd:\PythonScript\Devman\RomeoAndJuliet.txt')
+    get_most_frequent_words(file)
+
